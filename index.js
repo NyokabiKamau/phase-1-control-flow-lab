555c0f602dec4de6ac9467c0c5d100cf50845bff
NyokabiKamau <nyokabikamau2014@gmail.com>
2022-11-21 21:31:38 +0300

    pending completion

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(ride){
+  let message
+  if (ride <= 400) {
+    return "This one is on me!"
+  }
+  else if (ride > 2000 && ride <= 2500) {
+    return "I will gladly take your thirty bucks."
+  }
+  else if (ride > 2500) {
+    return "No can do."
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(cityName){
+  let city = (cityName = "NYC") ? "Ok, sounds good." : "No go.";
+  return city;
 }
 
 function switchOnCharmFromTip(){
-  // Write your code here!
+  let charm = "generous"
+  let charmMessage;
+
+  switch (charm) {
+    case "generous":
+      charmMessage = "Thank you so much."
+      break;
+    case "not as generous":
+      charmMessage = "Thank you"
+      break;
+    case "thanks for everything":
+      charmMessage = "Bye"
+      break;
+  }
 }
